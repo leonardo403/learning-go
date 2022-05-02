fd426efb6ab3d555e5cad0920d4e3c463ab43ad8
Leonardo Lima <leonardo@golaw.com.br>
2022-05-02 16:35:12 -0300

    Add data

--- data-access/main.go
+++ data-access/main.go
@@ -53,6 +53,16 @@ func main() {
         log.Fatal(err)
     }
     fmt.Printf("Album found: %v\n", alb)
+
+    albID, err := addAlbum(Album{
+        Title:  "The Modern Sound of Betty Carter",
+        Artist: "Betty Carter",
+        Price:  49.99,
+    })
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Printf("ID of added album: %v\n", albID)
 }
 
 // albumsByArtist queries for albums that have the specified artist name.
@@ -93,4 +103,18 @@ func albumByID(id int64) (Album, error) {
         return alb, fmt.Errorf("albumsById %d: %v", id, err)
     }
     return alb, nil
+}
+
+// addAlbum adds the specified album to the database,
+// returning the album ID of the new entry
+func addAlbum(alb Album) (int64, error) {
+    result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?, ?, ?)", alb.Title, alb.Artist, alb.Price)
+    if err != nil {
+        return 0, fmt.Errorf("addAlbum: %v", err)
+    }
+    id, err := result.LastInsertId()
+    if err != nil {
+        return 0, fmt.Errorf("addAlbum: %v", err)
+    }
+    return id, nil
 }
