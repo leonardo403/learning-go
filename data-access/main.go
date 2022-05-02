6dfba459789e6f3269a06c2863f674d549a57c0a
Leonardo Lima <leonardo@golaw.com.br>
2022-05-02 14:18:52 -0300

    Query for multiple rows

--- data-access/main.go
+++ data-access/main.go
@@ -11,6 +11,13 @@ import (
 
 var db *sql.DB
 
+type Album struct {
+    ID     int64
+    Title  string
+    Artist string
+    Price  float32
+}
+
 func main() {
     // Capture connection properties.
     cfg := mysql.Config{
@@ -33,4 +40,35 @@ func main() {
         log.Fatal(pingErr)
     }
     fmt.Println("Connected!")
+
+    albums, err := albumsByArtist("Gerry Mulligan")
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Printf("Albums found: %v\n", albums)
+}
+
+// albumsByArtist queries for albums that have the specified artist name.
+func albumsByArtist(name string) ([]Album, error) {
+    // An albums slice to hold data from returned rows.
+    var albums []Album
+
+    rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+    if err != nil {
+        return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+    }
+    defer rows.Close()
+    // Loop through rows, using Scan to assign column data to struct fields.
+    for rows.Next() {
+        var alb Album
+        if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+            return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+        }
+        albums = append(albums, alb)
+    }
+    if err := rows.Err(); err != nil {
+        return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+    }
+
+    return albums, nil
 }
